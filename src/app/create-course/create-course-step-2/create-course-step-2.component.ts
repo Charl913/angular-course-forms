a393be20ecaf94dab93e445da6690232778dcaba
Charl913 <charlkruger61@gmail.com>
2023-09-18 09:56:23 +0200

    Implement Angular Reactive Forms - Part 5

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -8,12 +8,33 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['create-course-step-2.component.scss']
 })
 export class CreateCourseStep2Component implements OnInit {
+  form = this.fb.group({
+    courseType: ['premium', Validators.required],
+    price: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern("[0-9]")
+    ]],
+    promoStartAt: [null],
+    promoEndAt: [null]
+  });
 
+  constructor(private fb: FormBuilder) {
 
+  }
   ngOnInit() {
+    this.form.valueChanges
+      .subscribe(val => {
+        const priceControl = this.form.controls['price'];
 
-
-
+        if(val.courseType == 'free' && priceControl.enabled) {
+          priceControl.disable({emitEvent: false});
+        }
+        else if(val.courseType == 'premium' && priceControl.disabled) {
+          priceControl.enable({emitEvent: false});
+        }
+      })
   }
 
 }
